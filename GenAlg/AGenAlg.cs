9810c394531d9ced4d12d4ca935352062da40dab
Doronin <rdoronin@merann.ru>
2019-04-02 11:33:35 +0300

    Add struct of gen alg

---
+++ GenAlg/AGenAlg.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneticAlgorithms.GenAlg
+{
+    abstract class AGenAlg
+    {
+        public abstract ISolution Solve(ITask task);
+
+        // Создание начальной популяции
+        protected abstract IPopulation doCreatePopulation();
+
+        // Отбор
+        protected abstract IPopulation doSelect(IPopulation population);
+
+        // Проверка на достижение результата
+        protected abstract bool doStop(IPopulation population);
+
+        // Отбор родителей
+        protected abstract IPopulation doBreed(IPopulation population);
+
+        // Скрещивание
+        protected abstract IPopulation doCross(IPopulation population);
+
+        // Мутация
+        protected abstract IPopulation doMutation(IPopulation population);
+    }
+}
---
+++ GenAlg/GenAlgTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneticAlgorithms.GenAlg
+{
+    class GenAlgTest : AGenAlg
+    {
+        public override ISolution Solve(ITask task)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IPopulation doBreed(IPopulation population)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IPopulation doCreatePopulation()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IPopulation doCross(IPopulation population)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IPopulation doMutation(IPopulation population)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IPopulation doSelect(IPopulation population)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool doStop(IPopulation population)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
